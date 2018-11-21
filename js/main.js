46f2dbd638529cf9d217b4ab846043b00d5e165d
Javier Bull <javierbullreyes@gmail.com>
2018-11-21 14:24:09 -0500

    validacion

--- js/main.js
+++ js/main.js
@@ -19,3 +19,30 @@ $('#blogCarousel').carousel({
   $('#carouselContent').carousel({
         interval: 3500
     });
+
+
+//validacion de formulario
+
+function validar(){
+
+    var nombre,correo,mensaje;
+    nombre=document.getElementById("name").value;
+email=document.getElementById("email").value;
+    mensaje=document.getElementById("mensaje").value;
+
+    if(nombre === "")
+{
+   alert("El campo del nombre esta vacío");
+    document.getElementById("name").focus();
+    return false;
+    }else if(email=== ""){
+     alert("El campo del correo esta vacío");
+     document.getElementById("email").focus();
+    return false;
+                }else if(mensaje=== ""){
+     alert("El campo del mensaje esta vacío");
+     document.getElementById("mensaje").focus();
+    return false;
+                }
+}
+//fin fomrulario

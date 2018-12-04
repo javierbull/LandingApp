d387f72dd6d2fb4165046dc8964e68e8d70b251e
Javier Bull <javierbullreyes@gmail.com>
2018-12-04 09:47:06 -0500

    carousel

--- js/main.js
+++ js/main.js
@@ -46,3 +46,27 @@ email=document.getElementById("email").value;
                 }
 }
 //fin fomrulario
+
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false,
+        margin: 20
+      }
+    }
+  })
+})
